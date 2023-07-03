0c990bd3627a80f27d4c983b725883186b9e96b5
Bhushan Patil <brpatil91299@gmail.com>
2023-07-03 17:15:26 +0530

    First commit

---
+++ UserManagementSystemWithValidation/UserManagementSystemWithValidation/src/main/java/com/example/UserManagementSystemWithValidationApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+
+UserManagementSystemWithValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementSystemWithValidationApplication.class, args);
+	}
+
+}
